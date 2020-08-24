04213c7e54404a4adc255d7fd64776206c72e244
Lucho-dev <luis.alberto.gomez@correounivalle.edu.co>
2020-08-24 18:14:37 -0500

    feat(last_changs): temaplates customs

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -205,7 +205,8 @@ CMS_TEMPLATES = (
     # Customize this
     ('fullwidth.html', 'Fullwidth'),
     ('sidebar_left.html', 'Sidebar Left'),
-    ('sidebar_right.html', 'Sidebar Right')
+    ('sidebar_right.html', 'Sidebar Right'),
+    ('home.html', 'home_cdap')
 )
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
